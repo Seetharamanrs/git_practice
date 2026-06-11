f2d26d834f380512cbadde5c07a856d353a6d7e3
seetharamanrs <seetharaman2632001@gmail.com>
2026-06-11 21:04:02 +0530

    The new Anagram is added

---
+++ anagram.py
@@ -0,0 +1,19 @@
+#anagram
+a='listen'
+b='silent'
+c={}
+d={}
+for i in a:
+    if i in d:
+        d[i]+=1
+    else:
+        d[i]=1
+for j in b:
+    if i in c:
+        c[i]+=1
+    else:
+        c[i]=1
+if c==d:
+    print("ANAGRAM")
+else:
+    print("NOT ANAGRAM")
